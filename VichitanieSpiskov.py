001f699e3c0e1c2edfd6b70fe71aac719f31bed4
findias <findias@bk.ru>
2018-12-01 15:42:27 +0300

    Оптимизировал код: Создал функцию для записи. Теперь в нее можно закидывать всякое

--- VichitanieSpiskov.py
+++ VichitanieSpiskov.py
@@ -1,22 +1,23 @@
 def read_spisok_first(a): # Функция собирает данные из файла
     with open(a, 'r') as f:    # Нежно читаем файл с  обязательным закрытия оного
         data = f.read() # Читаем файл и загоняем содержимое в str переменной data
-        data = data.replace('\t', ",").replace('\n', '').upper().split(',') # Легкое порно: pаменяем знаки табуляции запятыми, а знаки переносов убираем вовсе. Все буквы делаем заглавными. Назначаем запятую разделителем всего этого боголепия, что незамедлительно превращает данные в список.
+        data = data.replace('\t', ",").replace('\n', '').upper().split(',') # Легкое порно: меняем знаки табуляции запятыми, а знаки переносов убираем вовсе. Все буквы делаем заглавными. Назначаем запятую разделителем всего этого боголепия, что незамедлительно превращает данные в список.
         return data
+
+def write_to_file(filename, result): # Функция организовывает запись в файл
+    with open(filename, 'w') as f:
+        print(*result, file = f, sep = "," "\n") # Пишем в файл  результат
 
 a = 'first.txt'
 first_list = read_spisok_first(a)
-a = 'Second.txt'
+a = 'second.txt'
 second_list = read_spisok_first(a)
 
 first_minus_sesond = list(set(first_list) - set(second_list)) # Из списка First вычитаем список Second
 second_minus_first = list(set(second_list) - set(first_list)) # Из списка  Second Вычитаем список First
 
-with open('ResultFirstMinusSecond.txt', 'w') as f:
-    print(*first_minus_sesond, file = f, sep = "," "\n") # Пишем в файл "ResultFirstMinusSecond.txt" результат first_minus_second
-
-with open('ResultSecondMinusFirst.txt', 'w') as f:
-    print(*second_minus_first, file = f, sep = "," "\n") # Пишем в файл "ResultSecondMinusFirst.txt" результат second_minus_first
+write_to_file('ResultFirstMinusSecond.txt', first_minus_sesond)
+write_to_file('ResultSecondMinusFirst.txt', second_minus_first)
 
 import shutil # Подключаем модуль
 shutil.copy(r'ResultFirstMinusSecond.txt', r'1\ResultFirstMinusSecond.txt') #Копируемый файл для дальнейшей магии
