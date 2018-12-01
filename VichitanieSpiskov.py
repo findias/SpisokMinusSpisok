be24de102512f541782c3f55e3d8c98bc0a829bf
findias <findias@bk.ru>
2018-12-01 12:17:52 +0300

    Оптимизировал код оставил одну функцию вместо двух

--- VichitanieSpiskov.py
+++ VichitanieSpiskov.py
@@ -1,20 +1,16 @@
-def read_spisok_first(): # Функция собирает данные из файла first
-    with open('first.txt', 'r') as f:    # Нежно читаем файл с  обязательным закрытия оного
+def read_spisok_first(a): # Функция собирает данные из файла
+    with open(a, 'r') as f:    # Нежно читаем файл с  обязательным закрытия оного
         data = f.read() # Читаем файл и загоняем содержимое в str переменной data
-        data = data.replace('\t', ",").replace('\n', '').upper().split(',') # Легкое порно. Заменяем знаки табуляции запятыми, а знаки переносов убираем вовсе. Все буквы делаем заглавными. Назначаем запятую разделителем всего этого боголепия, что незамедлительно превращает данные в список.
+        data = data.replace('\t', ",").replace('\n', '').upper().split(',') # Легкое порно: pаменяем знаки табуляции запятыми, а знаки переносов убираем вовсе. Все буквы делаем заглавными. Назначаем запятую разделителем всего этого боголепия, что незамедлительно превращает данные в список.
         return data
 
-def read_spisok_second(): # Функция собирает данные из файла Second
-    with open('Second.txt', 'r') as f:
-        data = f.read()
-        data = data.replace('\t', ",").replace('\n', '').upper().split(',')
-        return data
+a = 'first.txt'
+first_list = read_spisok_first(a)
+a = 'Second.txt'
+second_list = read_spisok_first(a)
 
-first_minus_sesond = list(set(read_spisok_first()) - set(read_spisok_second()))# Из списка First вычитаем список Second
-second_minus_first = list(set(read_spisok_second()) - set(read_spisok_first()))# Из списка  Second Вычитаем список First
-
-first_minus_sesond = sorted(first_minus_sesond) #Делаем сортировку по возростания, дабы причинить богу сердечную радость, а войску веселье.
-second_minus_first = sorted(second_minus_first)
+first_minus_sesond = list(set(first_list) - set(second_list)) # Из списка First вычитаем список Second
+second_minus_first = list(set(second_list) - set(first_list)) # Из списка  Second Вычитаем список First
 
 with open('ResultFirstMinusSecond.txt', 'w') as f:
     print(*first_minus_sesond, file = f, sep = "," "\n") # Пишем в файл "ResultFirstMinusSecond.txt" результат first_minus_second
