03d163c5eabb3a61765b83f1e4a25de23450491c
findias <findias@bk.ru>
2018-12-03 19:44:48 +0300

    Добавлена фильтрация итоговых списков

--- vichitanie_spiskov.py
+++ vichitanie_spiskov.py
@@ -1,4 +1,5 @@
-import shutil # Подключаем модуль
+import shutil # Подключаем модули
+import re
 
 def read_spisok_first(a): # Функция собирает данные из файла
     with open(a, 'r') as f:    # Нежно читаем файл с  обязательным закрытия оного
@@ -11,6 +12,17 @@ def write_to_file(filename, result): # Функция организовывае
         for line in result:
             f.write("{},\n".format(line)) # Пишем в файл  результат
 
+def list_filtering(listname): # Функция фильтрует список, оставляя в только интересующие значания.
+    filter_list = []
+    for stroke in listname:
+        if re.findall(r'PC01', stroke):
+            filter_list.append(stroke)
+        if re.findall(r'S01', stroke):
+            filter_list.append(stroke)
+        if re.findall(r'RCTR2BR6', stroke):
+            filter_list.append(stroke)
+    return filter_list
+
 a = 'first.txt'
 first_list = read_spisok_first(a)
 a = 'second.txt'
@@ -19,7 +31,10 @@ second_list = read_spisok_first(a)
 first_minus_sesond = sorted(list(set(first_list) - set(second_list))) # Из списка First вычитаем список Second. И сортируем по возрастанию
 second_minus_first = sorted(list(set(second_list) - set(first_list))) # Из списка  Second Вычитаем список First. И сортируем по возрастанию
 
-write_to_file('ResultFirstMinusSecond2.txt', first_minus_sesond)
-write_to_file('ResultSecondMinusFirst2.txt', second_minus_first)
+filter_first_minus_sesond = list_filtering(first_minus_sesond)
+filter_second_minus_first = list_filtering(second_minus_first)
+
+write_to_file('ResultFirstMinusSecond.txt', filter_first_minus_sesond)
+write_to_file('ResultSecondMinusFirst.txt', filter_second_minus_first)
 
 shutil.copy(r'ResultFirstMinusSecond2.txt', r'1\ResultFirstMinusSecond.txt') #Копируемый файл для дальнейшей магии
