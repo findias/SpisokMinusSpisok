e5d8d822c36131ed1749f1e7023c3812e5acc6c2
findias <findias@bk.ru>
2018-12-03 19:00:12 +0300

    Оптимизировал код: изменено название файла

---
+++ vichitanie_spiskov.py
@@ -0,0 +1,25 @@
+import shutil # Подключаем модуль
+
+def read_spisok_first(a): # Функция собирает данные из файла
+    with open(a, 'r') as f:    # Нежно читаем файл с  обязательным закрытия оного
+        data = f.read() # Читаем файл и загоняем содержимое в str переменной data
+        data = data.replace('\t', "").upper().splitlines() # Легкое порно: меняем знаки табуляции запятыми, а знаки переносов убираем вовсе. Все буквы делаем заглавными. Убираем пробелы вконце и вначале. Назначаем запятую разделителем всего этого боголепия, что незамедлительно превращает данные в список.
+        return data
+
+def write_to_file(filename, result): # Функция организовывает запись в файл
+    with open(filename, 'w') as f:
+        for line in result:
+            f.write("{},\n".format(line)) # Пишем в файл  результат
+
+a = 'first.txt'
+first_list = read_spisok_first(a)
+a = 'second.txt'
+second_list = read_spisok_first(a)
+
+first_minus_sesond = sorted(list(set(first_list) - set(second_list))) # Из списка First вычитаем список Second. И сортируем по возрастанию
+second_minus_first = sorted(list(set(second_list) - set(first_list))) # Из списка  Second Вычитаем список First. И сортируем по возрастанию
+
+write_to_file('ResultFirstMinusSecond2.txt', first_minus_sesond)
+write_to_file('ResultSecondMinusFirst2.txt', second_minus_first)
+
+shutil.copy(r'ResultFirstMinusSecond2.txt', r'1\ResultFirstMinusSecond.txt') #Копируемый файл для дальнейшей магии
